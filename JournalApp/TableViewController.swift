9a0faed4795cd615dc60ce7b4b461fb7f63c93b7
David Brannen <david.brannen@Mobileappscompany@us>
2019-12-24 09:07:37 -0600

    recommit

--- JournalApp/TableViewController.swift
+++ JournalApp/TableViewController.swift
@@ -9,26 +9,6 @@
 import UIKit
 import CoreData
 
-struct Weather : Codable {
-    let consolidated_weather : [CityDayWeather]
-}
-struct CityDayWeather : Codable {
-    let id                  :  Int
-    let weather_state_name  :  String
-    let weather_state_abbr  :  String
-    let wind_direction_compass:String
-    let created             :  String
-    let applicable_date     :  String
-    let min_temp            :  Float
-    let max_temp            :  Float
-    let the_temp            :  Float
-    let wind_speed          :  Float
-    let wind_direction      :  Float
-    let air_pressure        :  Float
-    let humidity            :  Int
-    let visibility          :  Float?
-    let predictability      :  Int
-}
 
 class TableViewController: UITableViewController {
     let cellId = "Cell"
---
+++ Model/WeatherModel.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherModel.swift
+//  JournalApp
+//
+//  Created by MAC on 12/24/19.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import Foundation
+
+struct Weather : Codable {
+    let consolidated_weather : [CityDayWeather]
+}
+struct CityDayWeather : Codable {
+    let id                  :  Int
+    let weather_state_name  :  String
+    let weather_state_abbr  :  String
+    let wind_direction_compass:String
+    let created             :  String
+    let applicable_date     :  String
+    let min_temp            :  Float
+    let max_temp            :  Float
+    let the_temp            :  Float
+    let wind_speed          :  Float
+    let wind_direction      :  Float
+    let air_pressure        :  Float
+    let humidity            :  Int
+    let visibility          :  Float?
+    let predictability      :  Int
+}
